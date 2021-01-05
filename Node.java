e2f127b8e97f2aac08692957ac6580c1be049e13
jzhowie <jzhou20@stuy.edu>
2021-01-05 14:18:36 -0500

    Initialized Node class

---
+++ Node.java
@@ -0,0 +1,21 @@
+//TODO: get/set methods
+//ISSUES: TBD
+//NOTES:
+
+public class Node {
+
+public Node(String value){
+	data = value;
+	next = null;
+	prev = null;
+}
+
+private String data;
+private Node next,prev;
+
+public String getData() {
+	return data;
+}
+
+//getNext and prev here
+}
