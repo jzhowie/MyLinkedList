621063b949f4a4f1da1a9dfe876679aab14af104
jzhowie <jzhou20@stuy.edu>
2021-01-05 14:24:30 -0500

    Initialized LinkedList class (constructor and size() method only)

---
+++ MyLinkedList.java
@@ -0,0 +1,40 @@
+//TODO:
+//ISSUES: TBD
+//NOTES: Prioritize toString once you get the list working, helper methods returning Nodes must be private
+
+public class MyLinkedList {
+
+private int size;
+private Node start,end;
+
+public MyLinkedList() {
+	size = 0;
+	start = null;
+	end = null;
+}
+
+public int size() {
+	return size;
+}
+
+public boolean add(String value) {
+	return true;
+}
+
+public boolean add(int index, String value) {
+	return true;
+}
+
+public String get(int index) {
+	return "";
+}
+
+public String set(int index, String value) {
+	return "";
+}
+
+public String toString() {
+	return "";
+}
+
+}
